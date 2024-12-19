7d43d9f14913f9c61ef7fbb118e25b66a2585023
Thomas Babtist <thomas.babtist@sinch.com>
2024-12-19 18:05:54 +0100

    WIP

--- lambdas/auth/main.go
+++ lambdas/auth/main.go
@@ -1,277 +1,129 @@
 package main
 
 import (
-    "context"
-    "crypto/rsa"
-    "crypto/x509"
-    "encoding/base64"
-    "encoding/json"
-    "encoding/pem"
-    "fmt"
-    "log"
-    "math/big"
-    "strings"
-    "time"
+	"fmt"
+	"log"
+	"time"
 
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/dynamodb"
-    "github.com/golang-jwt/jwt/v4"
-    "github.com/google/uuid"
-    "github.com/go-resty/resty/v2"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/google/uuid"
 )
 
 var (
-    db                 *dynamodb.DynamoDB
-    googleCertsURL     = "https://www.googleapis.com/oauth2/v3/certs"
-    usersTableName     = "Users"
-    region             = "eu-north-1"
+	db             *dynamodb.DynamoDB
+	googleCertsURL = "https://www.googleapis.com/oauth2/v3/certs"
+	usersTableName = "Users"
+	region         = "eu-north-1"
 )
 
 func init() {
-    sess, err := session.NewSession(&aws.Config{
-        Region: aws.String(region),
-    })
-    if err != nil {
-        log.Fatalf("Failed to create AWS session: %v", err)
-    }
-    db = dynamodb.New(sess)
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
+	if err != nil {
+		log.Fatalf("Failed to create AWS session: %v", err)
+	}
+	db = dynamodb.New(sess)
 }
 
-type GoogleCert struct {
-    Keys []struct {
-        Kid string   `json:"kid"`
-        Kty string   `json:"kty"`
-        Alg string   `json:"alg"`
-        Use string   `json:"use"`
-        X5c []string `json:"x5c"`
-        N   string   `json:"n"`  // Modulus for RSA key
-        E   string   `json:"e"`  // Exponent for RSA key
-    } `json:"keys"`
-}
+func HandleRequest(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("Received Event: %+v", event)
 
-type CustomClaims struct {
-    Email string `json:"email"`
-    jwt.RegisteredClaims
-}
+	var userEmail string
+	var name string
+	authorizer := event.RequestContext.Authorizer
 
-func HandleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
-    log.Printf("Received Event: %+v", event)
+	if email, ok := authorizer["email"].(string); ok {
+		userEmail = email
+		name = authorizer["given_name"].(string) + " " + authorizer["family_name"].(string)
+	} else if claims, ok := authorizer["claims"].(map[string]interface{}); ok {
+		// Handle custom claims (if your Authorizer outputs claims in Payload V2.0)
+		if emailClaim, exists := claims["email"].(string); exists {
+			userEmail = emailClaim
+			name = claims["given_name"].(string) + " " + claims["family_name"].(string)
+		} else {
+			return events.APIGatewayProxyResponse{StatusCode: 401, Body: "Unauthorized: Email not found"}, nil
+		}
+	} else {
+		return events.APIGatewayProxyResponse{StatusCode: 401, Body: "Unauthorized"}, nil
+	}
 
-    idToken := event.AuthorizationToken
-    if idToken == "" {
-        log.Printf("Missing Authorization token")
-        return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("missing Authorization token")
-    }
+	err := storeUserIfNotExists(userEmail, name)
+	if err != nil {
+		log.Printf("Error storing user: %v", err)
+		return events.APIGatewayProxyResponse{}, fmt.Errorf("could not store user in DB")
+	}
 
-    if strings.HasPrefix(idToken, "Bearer ") {
-        idToken = idToken[7:]
-    }
-
-    log.Printf("Stripped Token: %s", idToken)  // Log stripped token
-    segments := strings.Split(idToken, ".")
-    if len(segments) != 3 {
-        log.Printf("Invalid JWT format, expected 3 segments")
-        return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("invalid JWT format, expected 3 segments")
-    }
-
-    certs, err := getGoogleCerts()
-    if err != nil {
-        log.Printf("Error fetching Google certs: %v", err)
-        return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("could not fetch Google certs")
-    }
-
-    log.Printf("Fetched Google certs: %+v", certs)  // Log the fetched certificates
-
-    claims, err := verifyGoogleToken(idToken, certs)
-    if err != nil {
-        log.Printf("Error verifying token: %v", err)
-        return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("invalid ID token")
-    }
-
-    log.Printf("Claims: %+v", claims)  // Log claims to inspect what the token contains
-    userEmail := claims.Email
-    err = storeUserIfNotExists(userEmail, claims.Subject)
-    if err != nil {
-        log.Printf("Error storing user: %v", err)
-        return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("could not store user in DB")
-    }
-
-    return generateAllowPolicy(userEmail), nil
-}
-
-func getGoogleCerts() (*GoogleCert, error) {
-    client := resty.New()
-    resp, err := client.R().Get(googleCertsURL)
-    if err != nil {
-        return nil, err
-    }
-
-    var certs GoogleCert
-    err = json.Unmarshal(resp.Body(), &certs)
-    if err != nil {
-        return nil, err
-    }
-
-    return &certs, nil
-}
-
-// Helper function to decode base64URL
-func base64URLDecode(input string) ([]byte, error) {
-    return base64.RawURLEncoding.DecodeString(input)
-}
-
-// Parse the RSA public key from the 'n' and 'e' fields
-func parseRSAPublicKey(nStr, eStr string) (*rsa.PublicKey, error) {
-    nBytes, err := base64URLDecode(nStr)
-    if err != nil {
-        return nil, fmt.Errorf("failed to decode n: %v", err)
-    }
-
-    eBytes, err := base64URLDecode(eStr)
-    if err != nil {
-        return nil, fmt.Errorf("failed to decode e: %v", err)
-    }
-
-    eInt := int(big.NewInt(0).SetBytes(eBytes).Int64())
-
-    pubKey := &rsa.PublicKey{
-        N: new(big.Int).SetBytes(nBytes),
-        E: eInt,
-    }
-    return pubKey, nil
-}
-
-func verifyGoogleToken(idToken string, certs *GoogleCert) (*CustomClaims, error) {
-    token, err := jwt.ParseWithClaims(idToken, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
-        kid, ok := token.Header["kid"].(string)
-        if !ok {
-            return nil, fmt.Errorf("missing kid in token header")
-        }
-
-        var publicKey interface{}
-        for _, key := range certs.Keys {
-            if key.Kid == kid {
-                // If X5c is populated, use the X5c cert, otherwise use n and e
-                if len(key.X5c) > 0 {
-                    publicKey = key.X5c[0]
-                    block, _ := pem.Decode([]byte(publicKey.(string)))
-                    if block == nil {
-                        return nil, fmt.Errorf("failed to parse PEM block containing the public key")
-                    }
-
-                    cert, err := x509.ParseCertificate(block.Bytes)
-                    if err != nil {
-                        return nil, fmt.Errorf("failed to parse certificate: %v", err)
-                    }
-
-                    return cert.PublicKey, nil
-                } else if key.N != "" && key.E != "" {
-                    // Use n and e for RSA
-                    return parseRSAPublicKey(key.N, key.E)
-                }
-            }
-        }
-
-        return nil, fmt.Errorf("unable to find matching cert")
-    })
-
-    if err != nil {
-        return nil, err
-    }
-
-    claims, ok := token.Claims.(*CustomClaims)
-    if !ok || !token.Valid {
-        return nil, fmt.Errorf("invalid token")
-    }
-
-    return claims, nil
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "",
+	}, nil
 }
 
 func storeUserIfNotExists(email string, name string) error {
-    userId := uuid.New().String()
+	userId := uuid.New().String()
 
-    user, err := getUserByEmail(email)
-    if err != nil {
-        log.Printf("Error checking user existence: %v", err)
-        return err
-    }
-    if user != nil {
-        return nil
-    }
+	user, err := getUserByEmail(email)
+	if err != nil {
+		log.Printf("Error checking user existence: %v", err)
+		return err
+	}
+	if user != nil {
+		return nil
+	}
 
-    _, err = db.PutItem(&dynamodb.PutItemInput{
-        TableName: aws.String(usersTableName),
-        Item: map[string]*dynamodb.AttributeValue{
-            "userId": {
-                S: aws.String(userId),
-            },
-            "email": {
-                S: aws.String(email),
-            },
-            "name": {
-                S: aws.String(name),
-            },
-            "createdAt": {
-                S: aws.String(time.Now().Format(time.RFC3339)),
-            },
-            "provider": {
-                S: aws.String("google"),
-            },
-        },
-    })
-    if err != nil {
-        log.Printf("Error storing user: %v", err)
-        return err
-    }
-    log.Printf("User %s stored successfully", email)
-    return nil
+	_, err = db.PutItem(&dynamodb.PutItemInput{
+		TableName: aws.String(usersTableName),
+		Item: map[string]*dynamodb.AttributeValue{
+			"userId": {
+				S: aws.String(userId),
+			},
+			"email": {
+				S: aws.String(email),
+			},
+			"name": {
+				S: aws.String(name),
+			},
+			"createdAt": {
+				S: aws.String(time.Now().Format(time.RFC3339)),
+			},
+			"provider": {
+				S: aws.String("google"),
+			},
+		},
+	})
+	if err != nil {
+		log.Printf("Error storing user: %v", err)
+		return err
+	}
+	log.Printf("User %s stored successfully", email)
+	return nil
 }
 
 func getUserByEmail(email string) (map[string]*dynamodb.AttributeValue, error) {
-    result, err := db.Query(&dynamodb.QueryInput{
-        TableName:              aws.String(usersTableName),
-        IndexName:              aws.String("email-userId-index"),
-        KeyConditionExpression: aws.String("email = :email"),
-        ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-            ":email": {
-                S: aws.String(email),
-            },
-        },
-    })
-    if err != nil {
-        return nil, err
-    }
-    if len(result.Items) == 0 {
-        return nil, nil
-    }
-    return result.Items[0], nil // Return the first item (if there are multiple)
-}
-
-
-func generateAllowPolicy(userId string) events.APIGatewayCustomAuthorizerResponse {
-    return events.APIGatewayCustomAuthorizerResponse{
-        PrincipalID:    userId,
-        PolicyDocument: generatePolicy("Allow"),
-    }
-}
-
-func generatePolicy(effect string) events.APIGatewayCustomAuthorizerPolicy {
-    return events.APIGatewayCustomAuthorizerPolicy{
-        Version:   "2012-10-17",
-        Statement: []events.IAMPolicyStatement{
-            {
-                Effect:   effect,
-                Action:   []string{"execute-api:Invoke"},
-                Resource: []string{"*"},
-            },
-        },
-    }
+	result, err := db.Query(&dynamodb.QueryInput{
+		TableName:              aws.String(usersTableName),
+		IndexName:              aws.String("email-userId-index"),
+		KeyConditionExpression: aws.String("email = :email"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":email": {
+				S: aws.String(email),
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(result.Items) == 0 {
+		return nil, nil
+	}
+	return result.Items[0], nil // Return the first item (if there are multiple)
 }
 
 func main() {
-    lambda.Start(HandleRequest)
+	lambda.Start(HandleRequest)
 }
-
--- lambdas/words/main.go
+++ lambdas/words/main.go
@@ -1,95 +1,226 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "log"
-    "strconv"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
 
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/dynamodb"
-    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 var (
-    db             *dynamodb.DynamoDB
-    wordsTableName = "Words" // Replace with your table name
-    region         = "eu-north-1"
+	db                  *dynamodb.DynamoDB
+	wordsTableName      = "Words"
+	usersTableName      = "Users"
+	statisticsTableName = "WordStatistics"
+	region              = "eu-north-1"
 )
 
 func init() {
-    sess, err := session.NewSession(&aws.Config{
-        Region: aws.String(region),
-    })
-    if err != nil {
-        log.Fatalf("Failed to create AWS session: %v", err)
-    }
-    db = dynamodb.New(sess)
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
+	if err != nil {
+		log.Fatalf("Failed to create AWS session: %v", err)
+	}
+	db = dynamodb.New(sess)
 }
 
 // Word represents a word entry in the DynamoDB table
 type Word struct {
-    Word      string `json:"word"`
-    Correct   string `json:"correct"`
-    Incorrect []string `json:"incorrect"`
+	Word      string   `json:"word"`
+	Correct   string   `json:"correct"`
+	Incorrect []string `json:"incorrect"`
 }
 
-// HandleRequest handles incoming API requests
+type WordResults struct {
+	Word      string `json:"word"`
+	IsCorrect bool   `json:"isCorrect"`
+}
+
+type WordStatistics struct {
+	UserId       string  `json:"userId"`
+	Word         string  `json:"word"`
+	Attempts     int     `json:"attempts"`
+	Success      int     `json:"success"`
+	SuccessRatio float32 `json:"successRatio"`
+}
+
 func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    numWordsStr := event.QueryStringParameters["numWords"]
-    if numWordsStr == "" {
-        return events.APIGatewayProxyResponse{StatusCode: 400, Body: "Missing numWords parameter"}, nil
-    }
+	method := event.RequestContext.HTTPMethod
+	switch method {
+	case "GET":
+		return handleGetWords(event)
+	case "POST":
+		return handleResults(event)
+	default:
+		return events.APIGatewayProxyResponse{StatusCode: 405, Body: "Method Not Allowed"}, nil
+	}
+}
 
-    numWords, err := strconv.Atoi(numWordsStr)
-    if err != nil || numWords <= 0 {
-        return events.APIGatewayProxyResponse{StatusCode: 400, Body: "Invalid numWords parameter"}, nil
-    }
+func handleGetWords(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	numWordsStr := event.QueryStringParameters["numWords"]
+	if numWordsStr == "" {
+		numWordsStr = "10"
+	}
 
-    words, err := getWords(numWords)
-    if err != nil {
-        log.Printf("Error retrieving words: %v", err)
-        return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Internal server error"}, nil
-    }
+	numWords, err := strconv.Atoi(numWordsStr)
+	if err != nil || numWords <= 0 {
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: "Invalid numWords parameter"}, nil
+	}
 
-    responseBody, err := json.Marshal(words)
-    if err != nil {
-        log.Printf("Error marshalling response: %v", err)
-        return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Internal server error"}, nil
-    }
+	words, err := getWords(numWords)
+	if err != nil {
+		log.Printf("Error retrieving words: %v", err)
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Internal server error"}, nil
+	}
 
-    return events.APIGatewayProxyResponse{
-        StatusCode: 200,
-        Body:       string(responseBody),
-    }, nil
+	responseBody, err := json.Marshal(words)
+	if err != nil {
+		log.Printf("Error marshalling response: %v", err)
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Internal server error"}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(responseBody),
+	}, nil
 }
 
 // getWords retrieves a specified number of words from DynamoDB
 func getWords(limit int) ([]Word, error) {
-    input := &dynamodb.ScanInput{
-        TableName: aws.String(wordsTableName),
-        Limit:     aws.Int64(int64(limit)),
-    }
+	input := &dynamodb.ScanInput{
+		TableName: aws.String(wordsTableName),
+		Limit:     aws.Int64(int64(limit)),
+	}
 
-    result, err := db.Scan(input)
-    if err != nil {
-        return nil, fmt.Errorf("failed to scan table: %w", err)
-    }
+	result, err := db.Scan(input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan table: %w", err)
+	}
 
-    var words []Word
-    err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &words)
-    if err != nil {
-        return nil, fmt.Errorf("failed to unmarshal words: %w", err)
-    }
+	var words []Word
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &words)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal words: %w", err)
+	}
 
-    return words, nil
+	return words, nil
+}
+
+func handleResults(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var userEmail string
+	authorizer := event.RequestContext.Authorizer
+
+	if email, ok := authorizer["email"].(string); ok {
+		userEmail = email
+	} else if claims, ok := authorizer["claims"].(map[string]interface{}); ok {
+		// Handle custom claims (if your Authorizer outputs claims in Payload V2.0)
+		if emailClaim, exists := claims["email"].(string); exists {
+			userEmail = emailClaim
+		} else {
+			return events.APIGatewayProxyResponse{StatusCode: 401, Body: "Unauthorized: Email not found"}, nil
+		}
+	} else {
+		return events.APIGatewayProxyResponse{StatusCode: 401, Body: "Unauthorized"}, nil
+	}
+	log.Printf("User email: %s", userEmail)
+
+	userId, err := getUserIdByEmail(userEmail)
+	if err != nil || userId == nil {
+		if err != nil {
+			log.Printf("Error getting user id: %v", err)
+		}
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: "User not found"}, nil
+	}
+
+	var wordResults []WordResults
+	err = json.Unmarshal([]byte(event.Body), &wordResults)
+	if err != nil {
+		log.Printf("Invalid request body: %v", err)
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: "Invalid request body"}, nil
+	}
+
+	// Process and update each word result
+	for _, result := range wordResults {
+		err := updateWordStatistics(*userId, result)
+		if err != nil {
+			log.Printf("Error updating word statistics: %v", err)
+			return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Failed to update statistics"}, nil
+		}
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Word resultss successfully uploaded",
+	}, nil
+}
+
+func getUserIdByEmail(email string) (*string, error) {
+	result, err := db.Query(&dynamodb.QueryInput{
+		TableName:              aws.String(usersTableName),
+		IndexName:              aws.String("email-userId-index"),
+		KeyConditionExpression: aws.String("email = :email"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":email": {
+				S: aws.String(email),
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(result.Items) == 0 {
+		return nil, nil
+	}
+	userId := result.Items[0]["userId"].S
+	return userId, nil
+}
+
+func updateWordStatistics(userId string, result WordResults) error {
+	// Define the primary key (userId and word)
+	key := map[string]*dynamodb.AttributeValue{
+		"userId": {S: aws.String(userId)},
+		"word":   {S: aws.String(result.Word)},
+	}
+
+	// Build the update expression
+	updateExpression := "SET attempts = if_not_exists(attempts, :zero) + :inc, " +
+		"success = if_not_exists(success, :zero) + :successInc, " +
+		"successRatio = (success + :successInc) / (attempts + :inc)"
+
+	// Define the expression attribute values
+	expressionValues := map[string]*dynamodb.AttributeValue{
+		":zero":       {N: aws.String("0")},
+		":inc":        {N: aws.String("1")},
+		":successInc": {N: aws.String("1")},
+	}
+	if !result.IsCorrect {
+		expressionValues[":successInc"] = &dynamodb.AttributeValue{N: aws.String("0")}
+	}
+
+	// Perform the update
+	_, err := db.UpdateItem(&dynamodb.UpdateItemInput{
+		TableName:                 aws.String(statisticsTableName),
+		Key:                       key,
+		UpdateExpression:          aws.String(updateExpression),
+		ExpressionAttributeValues: expressionValues,
+		ReturnValues:              aws.String("UPDATED_NEW"),
+	})
+	if err != nil {
+		log.Printf("Error updating item: %v", err)
+		return err
+	}
+	return nil
 }
 
 func main() {
-    lambda.Start(HandleRequest)
+	lambda.Start(HandleRequest)
 }
-

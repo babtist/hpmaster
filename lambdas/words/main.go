c68f48fecdb6fdea5132566e5476be9658a9c7a6
Thomas Babtist <thomas.babtist@sinch.com>
2024-12-20 01:26:06 +0100

    Add caching

--- lambdas/words/main.go
+++ lambdas/words/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -16,11 +20,17 @@ import (
 )
 
 var (
-	db                  *dynamodb.DynamoDB
-	wordsTableName      = "Words"
-	usersTableName      = "Users"
-	statisticsTableName = "WordStatistics"
-	region              = "eu-north-1"
+	db                 *dynamodb.DynamoDB
+	wordsTableName     = "Words"
+	usersTableName     = "Users"
+	wordStatsTableName = "WordStatistics"
+	region             = "eu-north-1"
+
+	userCache      map[string]string // In-memory cache for users (email->userId)
+	userCacheMutex sync.Mutex        // Mutex to protect userCache
+	cachedWords    map[string]Word
+	once           sync.Once
+	initErr        error
 )
 
 func init() {
@@ -31,9 +41,32 @@ func init() {
 		log.Fatalf("Failed to create AWS session: %v", err)
 	}
 	db = dynamodb.New(sess)
+
+	userCache = make(map[string]string)
+	cachedWords = make(map[string]Word)
+
+	words, err := fetchWordsFromDynamoDB()
+	if err != nil {
+		initErr = fmt.Errorf("Initialization error", err)
+		return
+	}
+	if len(words) == 0 {
+		initErr = fmt.Errorf("Failed to initialize the cache, no words available")
+	}
+	for _, word := range words {
+		cachedWords[word.Word] = word
+	}
+
 }
 
-// Word represents a word entry in the DynamoDB table
+type User struct {
+	UserId    string `json:"userId"`
+	Email     string `json:"email`
+	CreatedAt string `json:"createdAt"`
+	Name      string `json:"name"`
+	Provider  string `json:"provider"`
+}
+
 type Word struct {
 	Word      string   `json:"word"`
 	Correct   string   `json:"correct"`
@@ -54,6 +87,9 @@ type WordStatistics struct {
 }
 
 func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if initErr != nil {
+		log.Fatalf("Initialization failed: %v", initErr)
+	}
 	method := event.RequestContext.HTTPMethod
 	switch method {
 	case "GET":
@@ -65,7 +101,34 @@ func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (ev
 	}
 }
 
+// Shall only be called from init(). Not protected by mutex
+func fetchWordsFromDynamoDB() ([]Word, error) {
+
+	// Query your Words table here
+	input := &dynamodb.ScanInput{
+		TableName: aws.String(wordsTableName),
+	}
+
+	result, err := db.Scan(input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan table: %w", err)
+	}
+
+	var words []Word
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &words)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal words: %w", err)
+	}
+
+	return words, nil
+}
+
 func handleGetWords(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	userEmail, err := extractEmail(event)
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 401, Body: fmt.Sprint("%v", err)}, nil
+	}
+
 	numWordsStr := event.QueryStringParameters["numWords"]
 	if numWordsStr == "" {
 		numWordsStr = "10"
@@ -76,7 +139,15 @@ func handleGetWords(event events.APIGatewayProxyRequest) (events.APIGatewayProxy
 		return events.APIGatewayProxyResponse{StatusCode: 400, Body: "Invalid numWords parameter"}, nil
 	}
 
-	words, err := getWords(numWords)
+	userId, err := getUserIdByEmail(*userEmail)
+	if err != nil || userId == nil {
+		if err != nil {
+			log.Printf("Error getting user id: %v", err)
+		}
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: "User not found"}, nil
+	}
+
+	words, err := getWords(*userId, numWords)
 	if err != nil {
 		log.Printf("Error retrieving words: %v", err)
 		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Internal server error"}, nil
@@ -94,46 +165,120 @@ func handleGetWords(event events.APIGatewayProxyRequest) (events.APIGatewayProxy
 	}, nil
 }
 
-// getWords retrieves a specified number of words from DynamoDB
-func getWords(limit int) ([]Word, error) {
-	input := &dynamodb.ScanInput{
-		TableName: aws.String(wordsTableName),
-		Limit:     aws.Int64(int64(limit)),
+func getPoorPerformanceWords(userID string, limit int) ([]Word, error) {
+	// Query for poor performance words (userId = :userId) from WordStatistics table
+	performanceInput := &dynamodb.QueryInput{
+		TableName:              aws.String(wordStatsTableName),
+		IndexName:              aws.String("userId-successRatio-index"), // GSI on userId and successRatio
+		KeyConditionExpression: aws.String("userId = :userId"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":userId": {S: aws.String(userID)},
+		},
+		ScanIndexForward: aws.Bool(true),              // Sort in descending order (poorest first)
+		Limit:            aws.Int64(int64(limit / 2)), // Limit to half of the requested limit for poor performance words
 	}
 
-	result, err := db.Scan(input)
+	performanceResult, err := db.Query(performanceInput)
 	if err != nil {
-		return nil, fmt.Errorf("failed to scan table: %w", err)
+		return nil, fmt.Errorf("failed to query performance: %w", err)
 	}
 
-	var words []Word
-	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &words)
+	// Extract words from the result
+	var poorPerformanceWords []string
+	for _, item := range performanceResult.Items {
+		var wp WordStatistics
+		err = dynamodbattribute.UnmarshalMap(item, &wp)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal poor performance word: %w", err)
+		}
+		poorPerformanceWords = append(poorPerformanceWords, wp.Word)
+	}
+
+	// Fetch the complete Word objects from the Words table
+	var allPoorPerformanceWords []Word
+	for _, word := range poorPerformanceWords {
+		if completeWord, exists := cachedWords[word]; exists {
+			allPoorPerformanceWords = append(allPoorPerformanceWords, completeWord)
+		}
+	}
+
+	return allPoorPerformanceWords, nil
+}
+
+// Fetch random words
+func getRandomWords(limit int) []Word {
+
+	var randomWords []Word
+
+	// Initialize the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Initialize the reservoir to hold the first 'limit' words
+	i := 0
+	for _, word := range cachedWords {
+		if i < limit {
+			// Fill the reservoir with the first 'limit' words
+			randomWords = append(randomWords, word)
+		} else {
+			// Randomly replace an element in the reservoir with the new word
+			r := rand.Intn(i + 1)
+			if r < limit {
+				randomWords[r] = word
+			}
+		}
+		i++
+	}
+
+	return randomWords
+}
+
+func getWords(userID string, limit int) ([]Word, error) {
+	// Step 1: Fetch Poor Performance Words (with word details)
+	poorPerformanceWords, err := getPoorPerformanceWords(userID, limit)
 	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal words: %w", err)
+		return nil, err
 	}
 
-	return words, nil
+	// Step 2: Check if we have enough poor performance words
+	allWords := make([]Word, 0, limit)
+	seenWords := make(map[string]bool) // Map to track unique words
+
+	// Add poor performance words first
+	for _, word := range poorPerformanceWords {
+		if _, exists := seenWords[word.Word]; !exists {
+			allWords = append(allWords, word)
+			seenWords[word.Word] = true
+		}
+	}
+
+	// Step 3: If we don't have enough words, fetch random words
+	if len(allWords) < limit {
+		randomWords := getRandomWords(limit - len(allWords))
+
+		for _, word := range randomWords {
+			if _, exists := seenWords[word.Word]; !exists {
+				allWords = append(allWords, word)
+				seenWords[word.Word] = true
+			}
+		}
+	}
+
+	// Limit the result to the specified number of words
+	if len(allWords) > limit {
+		allWords = allWords[:limit]
+	}
+
+	return allWords, nil
 }
 
 func handleResults(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var userEmail string
-	authorizer := event.RequestContext.Authorizer
 
-	if email, ok := authorizer["email"].(string); ok {
-		userEmail = email
-	} else if claims, ok := authorizer["claims"].(map[string]interface{}); ok {
-		// Handle custom claims (if your Authorizer outputs claims in Payload V2.0)
-		if emailClaim, exists := claims["email"].(string); exists {
-			userEmail = emailClaim
-		} else {
-			return events.APIGatewayProxyResponse{StatusCode: 401, Body: "Unauthorized: Email not found"}, nil
-		}
-	} else {
-		return events.APIGatewayProxyResponse{StatusCode: 401, Body: "Unauthorized"}, nil
+	userEmail, err := extractEmail(event)
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 401, Body: fmt.Sprint("%v", err)}, nil
 	}
-	log.Printf("User email: %s", userEmail)
 
-	userId, err := getUserIdByEmail(userEmail)
+	userId, err := getUserIdByEmail(*userEmail)
 	if err != nil || userId == nil {
 		if err != nil {
 			log.Printf("Error getting user id: %v", err)
@@ -163,7 +308,39 @@ func handleResults(event events.APIGatewayProxyRequest) (events.APIGatewayProxyR
 	}, nil
 }
 
+func extractEmail(event events.APIGatewayProxyRequest) (*string, error) {
+	var userEmail string
+	authorizer := event.RequestContext.Authorizer
+
+	if email, ok := authorizer["email"].(string); ok {
+		userEmail = email
+	} else if claims, ok := authorizer["claims"].(map[string]interface{}); ok {
+		// Handle custom claims (if your Authorizer outputs claims in Payload V2.0)
+		if emailClaim, exists := claims["email"].(string); exists {
+			userEmail = emailClaim
+		} else {
+			return nil, errors.New("Unauthorized: Email not found")
+		}
+	} else {
+		return nil, errors.New("Unauthorized")
+	}
+	return &userEmail, nil
+
+}
+
 func getUserIdByEmail(email string) (*string, error) {
+	if userId, exists := userCache[email]; exists {
+		return &userId, nil // Return cached user
+	}
+
+	userCacheMutex.Lock()
+	defer userCacheMutex.Unlock()
+	// Check again after acquiring the lock (double-check locking)
+	// It's possible another goroutine already fetched the user in the meantime.
+	if userId, exists := userCache[email]; exists {
+		return &userId, nil // Return cached user (after the lock)
+	}
+
 	result, err := db.Query(&dynamodb.QueryInput{
 		TableName:              aws.String(usersTableName),
 		IndexName:              aws.String("email-userId-index"),
@@ -178,9 +355,10 @@ func getUserIdByEmail(email string) (*string, error) {
 		return nil, err
 	}
 	if len(result.Items) == 0 {
-		return nil, nil
+		return nil, errors.New("No user found")
 	}
 	userId := result.Items[0]["userId"].S
+	userCache[email] = *userId
 	return userId, nil
 }
 
@@ -191,31 +369,60 @@ func updateWordStatistics(userId string, result WordResults) error {
 		"word":   {S: aws.String(result.Word)},
 	}
 
+	var wordStats WordStatistics
+
+	resultItem, err := db.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(wordStatsTableName),
+		Key:       key,
+	})
+	if err != nil {
+		log.Printf("Error getting WordStatistics: %v", err)
+		return err
+	}
+
+	if resultItem.Item != nil {
+		err = dynamodbattribute.UnmarshalMap(resultItem.Item, &wordStats)
+		if err != nil {
+			log.Printf("Error unmarshalling result: %v", err)
+			return err
+		}
+	} else {
+		wordStats = WordStatistics{
+			Word:         result.Word,
+			UserId:       userId,
+			Attempts:     0,
+			Success:      0,
+			SuccessRatio: 0,
+		}
+	}
+	wordStats.Attempts++
+	if result.IsCorrect {
+		wordStats.Success++
+	}
+	wordStats.SuccessRatio = float32(wordStats.Success) / float32(wordStats.Attempts)
+
 	// Build the update expression
-	updateExpression := "SET attempts = if_not_exists(attempts, :zero) + :inc, " +
-		"success = if_not_exists(success, :zero) + :successInc, " +
-		"successRatio = (success + :successInc) / (attempts + :inc)"
+	updateExpression := "SET attempts = :attempts, " +
+		"success = :success, " +
+		"successRatio = :successRatio"
 
 	// Define the expression attribute values
 	expressionValues := map[string]*dynamodb.AttributeValue{
-		":zero":       {N: aws.String("0")},
-		":inc":        {N: aws.String("1")},
-		":successInc": {N: aws.String("1")},
-	}
-	if !result.IsCorrect {
-		expressionValues[":successInc"] = &dynamodb.AttributeValue{N: aws.String("0")}
+		":attempts":     {N: aws.String(fmt.Sprintf("%d", wordStats.Attempts))},
+		":success":      {N: aws.String(fmt.Sprintf("%d", wordStats.Success))},
+		":successRatio": {N: aws.String(fmt.Sprintf("%f", wordStats.SuccessRatio))},
 	}
 
 	// Perform the update
-	_, err := db.UpdateItem(&dynamodb.UpdateItemInput{
-		TableName:                 aws.String(statisticsTableName),
+	_, err = db.UpdateItem(&dynamodb.UpdateItemInput{
+		TableName:                 aws.String(wordStatsTableName),
 		Key:                       key,
 		UpdateExpression:          aws.String(updateExpression),
 		ExpressionAttributeValues: expressionValues,
 		ReturnValues:              aws.String("UPDATED_NEW"),
 	})
 	if err != nil {
-		log.Printf("Error updating item: %v", err)
+		log.Printf("Error updating WordStatistics: %v", err)
 		return err
 	}
 	return nil
